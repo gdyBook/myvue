9d266749361db0d6bb85c203529a272fd3c684af
gdy <1035215694@qq.com>
2017-09-21 10:07:53 +0800

    firsess

---
+++ src/components/footer.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div id="footers">
+    <h1>我是尾部</h1>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      author: "微信公众号 gdy"
+    }
+  }
+}
+</script>
+ 
+<style>
+#footers h1{
+	width: 100%;
+	height: 50px;
+	border-top: 1px solid #d8d8d8;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+}
+</style>
